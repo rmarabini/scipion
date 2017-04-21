41fc31abc56cd9650a159555585abfab242a3c57
rsanchez <rsanchez@cnb.csic.es>
2017-04-21 14:41:19 +0200

    1st draft of the protocol

--- pyworkflow/em/packages/xmipp3/protocol_screen_deeplearning1.py
+++ pyworkflow/em/packages/xmipp3/protocol_screen_deeplearning1.py
@@ -28,25 +28,15 @@
 from os.path import basename
 import numpy as np
 from math import ceil
+import os
 
 from pyworkflow.utils.path import copyFile, cleanPath 
 import pyworkflow.protocol.params as params
 from pyworkflow.em.protocol import ProtProcessParticles
-
 import pyworkflow.em.metadata as md
 from pyworkflow.em.packages.xmipp3.convert import writeSetOfParticles, setXmippAttributes
 import xmipp
 
-class DeepTFSupervised:
-    def read(self, fnNet):
-        pass
-
-    def write(self, fnNet):
-        pass
-    
-    def predict(self, I):
-        score = np.random.rand()
-        return score
 
 class XmippProtScreenDeepLearning1(ProtProcessParticles):
     """ Protocol for screening particles using deep learning. """
@@ -64,12 +54,14 @@ class XmippProtScreenDeepLearning1(ProtProcessParticles):
         form.addParam('inputParticles', params.PointerParam, label="Set of putative particles", 
                       pointerClass='SetOfParticles',
                       help='Select the set of possible particles.')
-        form.addParam('Nepochs', params.FloatParam, label="Number of epochs", default=3, expertLevel=params.LEVEL_ADVANCED,
+        form.addParam('Nepochs', params.FloatParam, label="Number of epochs", default=4.0, expertLevel=params.LEVEL_ADVANCED,
                       help='Number of epochs for the neural network training')  
-        form.addParam('learningRate', params.FloatParam, label="Learning rate", default=0.001, expertLevel=params.LEVEL_ADVANCED,
+        form.addParam('learningRate', params.FloatParam, label="Learning rate", default=0.0001, expertLevel=params.LEVEL_ADVANCED,
                       help='Learning rate of the neural network training')  
-                      
-        form.addParallelSection(threads=8, mpi=0)    
+        if os.environ['CUDA']:
+            form.addParallelSection(threads=0, mpi=0)           
+        else:
+            form.addParallelSection(threads=8, mpi=0)    
     
     
     #--------------------------- INSERT steps functions --------------------------------------------
@@ -93,36 +85,76 @@ class XmippProtScreenDeepLearning1(ProtProcessParticles):
         for id in mdAux:
             fnImage = mdAux.getValue(md.MDL_IMAGE, id)
             I.read(fnImage)
-            stack[...,n]=I.getData()
+            stack[n,...]= np.expand_dims(I.getData(),-1)
             n+=1
             if n>=batchSize:
                 break
-    
+    def estimateBatchSize(self,xdim,ydim):
+      if os.environ['CUDA']:
+        from tensorflow.python.client import device_lib
+        local_device_protos = device_lib.list_local_devices()
+        gpu_mem_list=[x.memory_limit for x in local_device_protos if x.device_type == 'GPU']
+        nBytes= int(0.2* gpu_mem_list[0]) #20% gpu memory for data. Rest for model
+        batchSize= nBytes // (xdim*ydim*4*64*3) # 64 is the max number of filters of model. 4 is float size. 3 for training requirements
+        print("BatchSize %d"%batchSize)
+        return batchSize
+      else:
+        return 4096
     def train(self, inputTrueParticles, inputNegatives, learningRate):
-        batchSize=100
+        from pyworkflow.em.packages.xmipp3.deepLearning1 import  DeepTFSupervised
+        numberOfThreads= None if os.environ['CUDA'] else self.numberOfThreads.get()
         mdTrue = md.MetaData(self._getExtraPath("trueParticles.xmd"))
         mdNegatives = md.MetaData(self._getExtraPath("negatives.xmd"))
         xdim, ydim, _ = inputTrueParticles.getDim()
         ndim = inputTrueParticles.getSize()
-        trueStack = np.zeros((xdim,ydim,batchSize))
-        negativeStack = np.zeros((xdim,ydim,batchSize))
-        batchNumber = int(ceil(ndim*self.Nepochs.get()/batchSize))
-        nnet = DeepTFSupervised()
-        for batch in range(batchNumber):
+        batchSize= self.estimateBatchSize(xdim,ydim)
+        trueStack = np.zeros((batchSize, xdim,ydim,1))
+        negativeStack = np.zeros((batchSize, xdim,ydim,1))
+        numberOfBatches = int(ceil(ndim*self.Nepochs.get()/batchSize))
+        nnet = DeepTFSupervised(rootPath=self._getExtraPath("nnetData"))
+        nnet.createNet(xdim, ydim, 1)
+        nnet.startSessionAndInitialize(numberOfThreads)
+        print("Training net for %d batches"%numberOfBatches)
+        for batch in range(numberOfBatches):
             self.getBatch(mdTrue, trueStack, batchSize) 
             self.getBatch(mdNegatives, negativeStack, batchSize)
-            nnet.write(self._getExtraPath("nnet.tf"))
-
+            nnet.trainNet(trueStack, negativeStack, numberOfBatches)
+        nnet.close(saveModel= True)
+        del nnet
     def predict(self, inputParticles):
-        nnet = DeepTFSupervised()
-        nnet.read(self._getExtraPath("nnet.tf"))
+        from pyworkflow.em.packages.xmipp3.deepLearning1 import  DeepTFSupervised
         mdParticles = md.MetaData(self._getPath("particles.xmd"))
+        xdim, ydim, _ = inputParticles.getDim()
+        ndim = inputParticles.getSize()
+                
+        nnet = DeepTFSupervised(rootPath=self._getExtraPath("nnetData"))
+        numberOfThreads= None if os.environ['CUDA'] else self.numberOfThreads.get()
+        nnet.createNet(xdim, ydim, 1)
+        nnet.startSessionAndInitialize(numberOfThreads)
+      
         I = xmipp.Image()
+        batchSize= self.estimateBatchSize(xdim,ydim)
+        stack = np.zeros((batchSize, xdim,ydim,1))
+        ids_list=[]
+        i=0
         for id in mdParticles:
             fnImage = mdParticles.getValue(md.MDL_IMAGE, id)
             I.read(fnImage)
-            score = nnet.predict(I.getData())
-            mdParticles.setValue(md.MDL_ZSCORE_DEEPLEARNING1, score, id)
+            stack[i, ...]= np.expand_dims(I.getData(),-1)
+            ids_list.append(id)
+            i+=1
+            if i==batchSize:
+                scores = nnet.predictNet(  stack )
+                assert len(scores)== len(ids_list)
+                for score, id in zip(scores,ids_list):
+                    mdParticles.setValue(md.MDL_ZSCORE_DEEPLEARNING1, float(score), id)
+                i=0
+                ids_list=[]                      
+        if i>0:                      
+            scores = nnet.predictNet(  stack[:i,...] )
+            assert len(scores)== len(ids_list)            
+            for score, id in zip(scores,ids_list):
+                mdParticles.setValue(md.MDL_ZSCORE_DEEPLEARNING1, float(score), id)                      
         mdParticles.write(self._getPath("particles.xmd"))
 
     def createOutputStep(self):
